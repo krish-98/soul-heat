bb735724ff0b779cb2d1aced2b2622f5f4550c3f
krish-98 <muralikrishnanm98@gmail.com>
2025-06-13 15:50:29 +0530

    Install necessary packages

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-cyan-500 underline underline-offset-4 decoration-dotted decoration-purple-500">
+        Hello
+      </h1>
+    </div>
+  )
+}

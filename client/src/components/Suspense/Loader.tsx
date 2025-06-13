225dee5a10679448dc3443a727c0cf39b8ad7862
krish-98 <muralikrishnanm98@gmail.com>
2025-06-13 14:03:48 +0530

    remove everything

--- client/src/components/Suspense/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import { BounceLoader } from 'react-spinners'
-
-export default function Loader() {
-  return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-[#f5f3f3] bg-opacity-50">
-      <BounceLoader color="#ffae3c" />
-    </div>
-  )
-}
